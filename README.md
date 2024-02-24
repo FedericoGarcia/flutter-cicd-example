efd26886424ae88179f5b5f4ed775c07283d7f23
Federico García <garciafedericoagustin@gmail.com>
2024-02-24 12:55:08 -0300

    Setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_cicd_example
+
+An app to demonstrate how to set up an automated deployment system on Google Play
---
+++ android/app/src/main/kotlin/sh/fede/flutter_cicd_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package sh.fede.flutter_cicd_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
